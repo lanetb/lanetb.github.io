4ced1c11445dc1a867f2d6342d069a27c950d34d
lanetb <lanetb@dukes.jmu.edu>
2023-11-22 13:27:14 -0500

    made gh-pages work

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	},
 	preprocess: vitePreprocess()
 };
